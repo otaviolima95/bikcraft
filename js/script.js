fcee1c151f0b05a2fbc6f3675acef4a6ebe31beb
Otávio Lima <otaviolima95@hotmail.com>
2023-05-22 09:35:01 -0300

    otimizado para producao

---
+++ js/script.js
@@ -0,0 +1,51 @@
+// Ativar links do menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+    const url = location.href;
+    const href = link.href;
+    if (url.includes(href)) {
+        link.classList.add("ativo")
+    }
+}
+
+links.forEach(ativarLink)
+
+//Ativar Itens do Orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if (elemento) {
+        elemento.checked = true;
+    }
+}
+
+parametros.forEach(ativarProduto);
+
+//Perguntas Frequentes
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
+    
+}
+
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener("click", ativarPergunta);
+    console.log(pergunta)
+}
+
+perguntas.forEach(eventosPerguntas);
+
+//Animação
+if (window.SimpleAnime) {
+new SimpleAnime();
+}
